593a3e60e6fd8a9bc1701dd3addcfae2aa462316
Rayane <rchaabane20@gmail.com>
2024-05-01 21:00:06 +0200

    Fichier JS

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+function openModal(imageSrc) {
+    var modal = document.getElementById("modal");
+    var modalImage = document.getElementById("modal-image");
+    modal.style.display = "block";
+    modalImage.src = imageSrc;
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
